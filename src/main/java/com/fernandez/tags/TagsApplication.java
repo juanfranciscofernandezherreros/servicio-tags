aa1329baca5a76cc8181aca854b7eacc10e08383
jffernandez.herreros <juanfrancisco.fernandezherreros@colaborador.viajeseci.es>
2021-01-11 03:02:16 +0100

    config : Dockerfile

--- src/main/java/com/fernandez/tags/TagsApplication.java
+++ src/main/java/com/fernandez/tags/TagsApplication.java
@@ -10,7 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableEurekaClient
 @ComponentScan("com.fernandez.tags.*")
 @EnableSwagger2
 public class TagsApplication {
